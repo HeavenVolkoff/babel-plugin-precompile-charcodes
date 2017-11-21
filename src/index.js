dc26ed86026f199781de217724c7fa06b9ff8c8b
Vítor Augusto da Silva Vasconcellos <HeavenVolkoff@users.noreply.github.com>
2017-11-21 00:05:26 -0200

    Updated lib to be compatible with Babel 7

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-function isCharCode (path) {
-    return (
-        path.node.property.name === 'charCodeAt' &&
-        path.parent.arguments[0].value === 0
-    );
-}
+'use strict'
 
-function replaceNode (t, path) {
-    if (!isCharCode(path)) {
-        return;
-    }
-    const {object} = path.node;
-    if (object.type === 'StringLiteral') {
-        return path.parentPath.replaceWith(
-            t.numericLiteral(path.node.object.value.charCodeAt(0))
-        );
-    }
-    if (object.type === 'TemplateLiteral' && !object.expressions.length) {
-        return path.parentPath.replaceWith(
-            t.numericLiteral(path.node.object.quasis[0].value.raw.charCodeAt(0))
-        );
-    }
-}
+exports.__esModule = true
+exports.default = _default
 
-export default function ({types: t}) {
-    return {
-        visitor: {
-            MemberExpression (path) {
-                return replaceNode(t, path);
-            },
-        },
-    };
+var core = require('@babel/core')
+
+function _default () {
+  var t = core.types
+
+  function isCharCode (path) {
+    return path.node.property.name === 'charCodeAt' && path.parent.arguments[0].value === 0
+  }
+
+  return {
+    visitor: {
+      MemberExpression: {
+        exit: function exit (path) {
+          if (!isCharCode(path)) {
+            return
+          }
+
+          var object = path.node.object
+          if (object.type === 'StringLiteral') {
+            path.parentPath.replaceWith(t.numericLiteral(path.node.object.value.charCodeAt(0)))
+          } else if (object.type === 'TemplateLiteral' && !object.expressions.length) {
+            path.parentPath.replaceWith(t.numericLiteral(path.node.object.quasis[0].value.raw.charCodeAt(0)))
+          }
+        }
+      }
+    }
+  }
 }
